f010eaf2c1a6e5c0cd2b32535643e6ccd97e915e
Lingrui <lingruicai@gmail.com>
2017-11-02 15:16:43 -0400

    update

--- fearture_engg.py
+++ fearture_engg.py
@@ -88,6 +88,9 @@ test_df["num_words_title"] = test_df["text"].apply(lambda x: len([w for w in str
 train_df["mean_word_len"] = train_df["text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
 test_df["mean_word_len"] = test_df["text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
 
+print(train_df.head())
+
+##number of words by author
 train_df['num_words'].loc[train_df['num_words']>80] = 80 #truncation for better visuals
 plt.figure(figsize=(12,8))
 sns.violinplot(x='author', y='num_words', data=train_df)
@@ -96,5 +99,280 @@ plt.ylabel('Number of words in text', fontsize=12)
 plt.title("Number of words by author", fontsize=15)
 plt.savefig("Number_of_words.png")
 #plt.show()
+
+
+#number of punctions by author
+train_df['num_punctuations'].loc[train_df['num_punctuations']>10] = 10 #truncation for better visuals
+plt.figure(figsize=(12,8))
+sns.violinplot(x='author', y='num_punctuations', data=train_df)
+plt.xlabel('Author Name', fontsize=12)
+plt.ylabel('Number of puntuations in text', fontsize=12)
+plt.title("Number of punctuations by author", fontsize=15)
+plt.savefig("Number_of_punctions.png")
+#plt.show()
+
+
+## Prepare the data for modeling ###
+author_mapping_dict = {'EAP':0, 'HPL':1, 'MWS':2}
+train_y = train_df['author'].map(author_mapping_dict)
+train_id = train_df['id'].values
+test_id = test_df['id'].values
+
+### recompute the trauncated variables again ###
+'''
+train_df["num_words"] = train_df["text"].apply(lambda x: len(str(x).split()))
+test_df["num_words"] = test_df["text"].apply(lambda x: len(str(x).split()))
+train_df["mean_word_len"] = train_df["text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+test_df["mean_word_len"] = test_df["text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+'''
+
+cols_to_drop = ['id', 'text']
+train_X = train_df.drop(cols_to_drop+['author'], axis=1)
+test_X = test_df.drop(cols_to_drop, axis=1)
+
+###########A simple XGBoost model####################
+def runXGB(train_X, train_y, test_X, test_y=None, test_X2=None, seed_val=0, child=1, colsample=0.3):
+    param = {}
+    param['objective'] = 'multi:softprob'
+    param['eta'] = 0.1
+    param['max_depth'] = 3
+    param['silent'] = 1
+    param['num_class'] = 3
+    param['eval_metric'] = "mlogloss"
+    param['min_child_weight'] = child
+    param['subsample'] = 0.8
+    param['colsample_bytree'] = colsample
+    param['seed'] = seed_val
+    num_rounds = 2000
+
+    plst = list(param.items())
+    xgtrain = xgb.DMatrix(train_X, label=train_y)
+
+    if test_y is not None:
+        xgtest = xgb.DMatrix(test_X, label=test_y)
+        watchlist = [ (xgtrain,'train'), (xgtest, 'test') ]
+        model = xgb.train(plst, xgtrain, num_rounds, watchlist, early_stopping_rounds=50, verbose_eval=20)
+    else:
+        xgtest = xgb.DMatrix(test_X)
+        model = xgb.train(plst, xgtrain, num_rounds)
+
+    pred_test_y = model.predict(xgtest, ntree_limit = model.best_ntree_limit)
+    if test_X2 is not None:
+        xgtest2 = xgb.DMatrix(test_X2)
+        pred_test_y2 = model.predict(xgtest2, ntree_limit = model.best_ntree_limit)
+    return pred_test_y, pred_test_y2, model
+
+
+####### k-fold cross validation###############
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0], 3])
+for dev_index, val_index in kf.split(train_X):
+    dev_X, val_X = train_X.loc[dev_index], train_X.loc[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runXGB(dev_X, dev_y, val_X, val_y, test_X, seed_val=0)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index,:] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+    break
+print("cv scores : ", cv_scores)
+
+pred_train_df = pd.DataFrame(pred_train)
+joined = pd.DataFrame(train_df["id"]).join(pred_train_df)
+joined.to_csv("predictions.csv",index=False)
+#####################################################
+
+#####################################################
+
+### Plot the important variables ###
+fig, ax = plt.subplots(figsize=(12,12))
+#xgb.plot_importance(model, max_num_features=50, height=0.8, ax=ax)
+xgb.plot_importance(model, height=0.8, ax=ax)
+plt.savefig("important_variables.png")
+#plt.show()
+
+######Text Based Features#######
+
+### Fit transform the tfidf vectorizer ###
+tfidf_vec = TfidfVectorizer(stop_words='english', ngram_range=(1,3))
+full_tfidf = tfidf_vec.fit_transform(train_df['text'].values.tolist() + test_df['text'].values.tolist())
+train_tfidf = tfidf_vec.transform(train_df['text'].values.tolist())
+test_tfidf = tfidf_vec.transform(test_df['text'].values.tolist())
+
+def runMNB(train_X, train_y, test_X, test_y, test_X2):
+    model = naive_bayes.MultinomialNB()
+    model.fit(train_X, train_y)
+    pred_test_y = model.predict_proba(test_X)
+    pred_test_y2 = model.predict_proba(test_X2)
+    return pred_test_y, pred_test_y2, model
+
+#Naive Bayes on Word Tfidf Vectorizer:
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0], 3])
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+for dev_index, val_index in kf.split(train_X):
+    dev_X, val_X = train_tfidf[dev_index], train_tfidf[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runMNB(dev_X, dev_y, val_X, val_y, test_tfidf)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index,:] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+print("Mean cv score : ", np.mean(cv_scores))
+pred_full_test = pred_full_test / 5.
+
+
+###SVD on word TFIDF:
+n_comp = 20
+svd_obj = TruncatedSVD(n_components=n_comp, algorithm='arpack')
+svd_obj.fit(full_tfidf)
+train_svd = pd.DataFrame(svd_obj.transform(train_tfidf))
+test_svd = pd.DataFrame(svd_obj.transform(test_tfidf))
+    
+train_svd.columns = ['svd_word_'+str(i) for i in range(n_comp)]
+test_svd.columns = ['svd_word_'+str(i) for i in range(n_comp)]
+train_df = pd.concat([train_df, train_svd], axis=1)
+test_df = pd.concat([test_df, test_svd], axis=1)
+del full_tfidf, train_tfidf, test_tfidf, train_svd, test_svd
+
+
+#Naive Bayes on Word Count Vectorizer:
+### Fit transform the count vectorizer ###
+tfidf_vec = CountVectorizer(stop_words='english', ngram_range=(1,3))
+tfidf_vec.fit(train_df['text'].values.tolist() + test_df['text'].values.tolist())
+train_tfidf = tfidf_vec.transform(train_df['text'].values.tolist())
+test_tfidf = tfidf_vec.transform(test_df['text'].values.tolist())
+
+
+#build Multinomial NB model using count vectorizer based features.
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0], 3])
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+for dev_index, val_index in kf.split(train_X):
+    dev_X, val_X = train_tfidf[dev_index], train_tfidf[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runMNB(dev_X, dev_y, val_X, val_y, test_tfidf)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index,:] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+print("Mean cv score : ", np.mean(cv_scores))
+pred_full_test = pred_full_test / 5.
+
+# add the predictions as new features #
+train_df["nb_cvec_eap"] = pred_train[:,0]
+train_df["nb_cvec_hpl"] = pred_train[:,1]
+train_df["nb_cvec_mws"] = pred_train[:,2]
+test_df["nb_cvec_eap"] = pred_full_test[:,0]
+test_df["nb_cvec_hpl"] = pred_full_test[:,1]
+test_df["nb_cvec_mws"] = pred_full_test[:,2]
+
+
+#Naive Bayes on Character Count Vectorizer
+### Fit transform the tfidf vectorizer ###
+tfidf_vec = CountVectorizer(ngram_range=(1,7), analyzer='char')
+tfidf_vec.fit(train_df['text'].values.tolist() + test_df['text'].values.tolist())
+train_tfidf = tfidf_vec.transform(train_df['text'].values.tolist())
+test_tfidf = tfidf_vec.transform(test_df['text'].values.tolist())
+
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0], 3])
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+for dev_index, val_index in kf.split(train_X):
+    dev_X, val_X = train_tfidf[dev_index], train_tfidf[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runMNB(dev_X, dev_y, val_X, val_y, test_tfidf)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index,:] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+print("Mean cv score : ", np.mean(cv_scores))
+pred_full_test = pred_full_test / 5.
+
+# add the predictions as new features #
+train_df["nb_cvec_char_eap"] = pred_train[:,0]
+train_df["nb_cvec_char_hpl"] = pred_train[:,1]
+train_df["nb_cvec_char_mws"] = pred_train[:,2]
+test_df["nb_cvec_char_eap"] = pred_full_test[:,0]
+test_df["nb_cvec_char_hpl"] = pred_full_test[:,1]
+test_df["nb_cvec_char_mws"] = pred_full_test[:,2]
+
+#Naive Bayes on Character Tfidf Vectorizer:
+### Fit transform the tfidf vectorizer ###
+tfidf_vec = TfidfVectorizer(ngram_range=(1,5), analyzer='char')
+full_tfidf = tfidf_vec.fit_transform(train_df['text'].values.tolist() + test_df['text'].values.tolist())
+train_tfidf = tfidf_vec.transform(train_df['text'].values.tolist())
+test_tfidf = tfidf_vec.transform(test_df['text'].values.tolist())
+
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0], 3])
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+for dev_index, val_index in kf.split(train_X):
+    dev_X, val_X = train_tfidf[dev_index], train_tfidf[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runMNB(dev_X, dev_y, val_X, val_y, test_tfidf)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index,:] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+print("Mean cv score : ", np.mean(cv_scores))
+pred_full_test = pred_full_test / 5.
+
+# add the predictions as new features #
+train_df["nb_tfidf_char_eap"] = pred_train[:,0]
+train_df["nb_tfidf_char_hpl"] = pred_train[:,1]
+train_df["nb_tfidf_char_mws"] = pred_train[:,2]
+test_df["nb_tfidf_char_eap"] = pred_full_test[:,0]
+test_df["nb_tfidf_char_hpl"] = pred_full_test[:,1]
+test_df["nb_tfidf_char_mws"] = pred_full_test[:,2]
+
+
+##SVD on Character TFIDF:
+
+n_comp = 20
+svd_obj = TruncatedSVD(n_components=n_comp, algorithm='arpack')
+svd_obj.fit(full_tfidf)
+train_svd = pd.DataFrame(svd_obj.transform(train_tfidf))
+test_svd = pd.DataFrame(svd_obj.transform(test_tfidf))
+    
+train_svd.columns = ['svd_char_'+str(i) for i in range(n_comp)]
+test_svd.columns = ['svd_char_'+str(i) for i in range(n_comp)]
+train_df = pd.concat([train_df, train_svd], axis=1)
+test_df = pd.concat([test_df, test_svd], axis=1)
+del full_tfidf, train_tfidf, test_tfidf, train_svd, test_svd
+
+###XGBoost model:
+
+cols_to_drop = ['id', 'text']
+train_X = train_df.drop(cols_to_drop+['author'], axis=1)
+test_X = test_df.drop(cols_to_drop, axis=1)
+
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0], 3])
+for dev_index, val_index in kf.split(train_X):
+    dev_X, val_X = train_X.loc[dev_index], train_X.loc[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runXGB(dev_X, dev_y, val_X, val_y, test_X, seed_val=0, colsample=0.7)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index,:] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+    break
+print("cv scores : ", cv_scores)
+
+out_df = pd.DataFrame(pred_full_test)
+out_df.columns = ['EAP', 'HPL', 'MWS']
+out_df.insert(0, 'id', test_id)
+out_df.to_csv("sub_fe.csv", index=False)
+
+
+
+
+
+
+
+
 
 

6f151d7d77429c6a1125dd7c24c646f71425b1b3
Lingrui <lingruicai@gmail.com>
2017-11-16 09:29:55 -0500

    update

--- spooky_LSTM.py
+++ spooky_LSTM.py
@@ -14,6 +14,8 @@ from keras.models import Model
 from keras.optimizers import *
 from keras.models import Sequential 
 from keras.layers import Merge 
+from keras import regularizers
+from keras.callbacks import EarlyStopping
 import sys
 
 import numpy as np
@@ -21,13 +23,8 @@ import pandas as pd
 
 MAX_SEQUENCE_LENGTH = 1000 #max words in a document
 MAX_NB_WORDS = 400000 #max words in a dictionary X_SEQUENCE_LENGTH
-EMBEDDING_DIM = 100 #dimention of word vector 
 VALIDATION_SPLIT = 0.2 #percentage of test data 
 
-# first, build index mapping words in the embeddings set
-# to their embedding vector
-
-# second, prepare text samples and their labels
 print('Processing text dataset')
 
 input_df = pd.read_csv("/home/lcai/s2/Halloween/input/train.csv")
@@ -39,19 +36,23 @@ tokenizer.fit_on_texts(np.asarray(input_df['text']))
 sequences = tokenizer.texts_to_sequences(np.asarray(input_df["text"]))
 
 word_index = tokenizer.word_index
-print('Found %s unique tokens.' % len(word_index))
+print('Found %s unique tokens in trainning data.' % len(word_index))
 
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
 labels = to_categorical(np.asarray(train_y))
 print('Shape of data tensor:', data.shape)
 print('Shape of label tensor:', labels.shape)
-x_train = data
+#x_train = data
+#y_train = labels
 ##prepare test data
 test_df = pd.read_csv("/home/lcai/s2/Halloween/input/test.csv")
-test.id = test_df['id'].values
+test_id = test_df['id'].values
+tokenizer.fit_on_texts(np.asarray(test_df['text']))
+test_sequences = tokenizer.texts_to_sequences(np.asarray(test_df["text"]))
+test_data = pad_sequences(test_sequences, maxlen=MAX_SEQUENCE_LENGTH)
+x_val = test_data
 
 # split the data into a training set and a validation set
-'''
 indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
 data = data[indices]
@@ -60,9 +61,8 @@ num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
 
 x_train = data[:-num_validation_samples]
 y_train = labels[:-num_validation_samples]
-x_val = data[-num_validation_samples:]
-y_val = labels[-num_validation_samples:]
-'''
+x_train_val = data[-num_validation_samples:]
+y_train_val = labels[-num_validation_samples:]
 
 print('Training model.')
 
@@ -70,32 +70,43 @@ model = Sequential()
 
 model.add(Embedding(MAX_NB_WORDS,128))
 model.add(SpatialDropout1D(0.2))
+
+model.add(Conv1D(activation='relu',padding='same',filters=64, kernel_size=5))
+model.add(MaxPooling1D(pool_size=4))
 ###LSTM layer 
 model.add(LSTM(128,recurrent_dropout=0.2,dropout=0.2))
-model.add(Dense(3,activation='softmax'))
+model.add(Dense(3,activation='softmax',kernel_regularizer=regularizers.l2(0.01),activity_regularizer=regularizers.l1(0.001)))
 #model.add(Activation('sigmoid'))
 model.add(Activation('relu'))
     
 model.summary()
 
 #try using different optimizers and different optimizer configs
+earlyStopping= EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')    
 model.compile(loss='categorical_crossentropy',
                 #optimizer='Adadelta',
                 optimizer='adam',
                 metrics=['accuracy'])
 
 model.fit(x_train,y_train,
+        validation_data=(x_train_val,y_train_val),
         batch_size=128,
         epochs=10,
+        callbacks=[earlyStopping],
         )
 #        validation_data=(x_val,y_val))
 
 score = model.evaluate(x_train,y_train,verbose=0)
 print('train score:',score[0])
 print('train accuracy:',score[1])
+'''
 score = model.evaluate(x_val,y_val,verbose=0)
 print('Test score:',score[0])
 print('Test accuracy:',score[1])
-
+'''
 classes = model.predict(x_val)
 print(classes[1])
+out_df = pd.DataFrame(classes)
+out_df.columns = ['EAP', 'HPL', 'MWS']
+out_df.insert(0, 'id', test_id)
+out_df.to_csv("Conv1D_LSTM.csv", index=False)
